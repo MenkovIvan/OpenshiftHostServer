1e5ce6440f0d120f5ac5a9d84972c1ce09fb969b
Ivan <menkov_99@mail.ru>
2019-05-29 14:12:39 +0300

    ivan
    fix

---
+++ src/main/java/servlets/servletUpdate.java
@@ -0,0 +1,42 @@
+package servlets;
+
+import database.CheckInformation;
+import database.UpdateInformation;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/api/update")
+public class servletUpdate extends HttpServlet {
+    public void init(ServletConfig servletConfig) {
+        try {
+            super.init(servletConfig);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream os = resp.getOutputStream();
+        String login = req.getParameter("login");
+        System.out.println("login: " + login);
+
+        try {
+            UpdateInformation.updateI_Invite(login,-1);
+            UpdateInformation.updateMe_Invite(-1,login);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        os.print("0");
+    }
+}
